ad546b052ac02bb6a4e13a907c34db50e4230953
Akshunn <trivediakshunn@gmail.com>
2026-05-21 19:41:20 -0400

    minor comments added for understanding

--- Examples/Kinova/SystemIdentification/ParametersIdentification/include/EndEffectorIdentificationMomentumPybindWrapper.h
+++ Examples/Kinova/SystemIdentification/ParametersIdentification/include/EndEffectorIdentificationMomentumPybindWrapper.h
@@ -8,8 +8,8 @@
 
 #include "EndEffectorParametersIdentificationMomentum.h"
 
-#include "pinocchio/parsers/urdf.hpp"
 #include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/parsers/urdf.hpp"
 
 namespace RAPTOR {
 namespace Kinova {
@@ -18,61 +18,60 @@ namespace nb = nanobind;
 
 class EndEffectorIdentificationMomentumPybindWrapper {
 public:
-    using Model = pinocchio::Model;
-    using Vec3 = Eigen::Vector3d;
-    using Vec10 = Eigen::Vector<double, 10>;
-    using VecX = Eigen::VectorXd;
-    using MatX = Eigen::MatrixXd;
+  using Model = pinocchio::Model;
+  using Vec3 = Eigen::Vector3d;
+  using Vec10 = Eigen::Vector<double, 10>;
+  using VecX = Eigen::VectorXd;
+  using MatX = Eigen::MatrixXd;
 
-    using nb_1d_double = nb::ndarray<double, nb::ndim<1>, nb::c_contig, nb::device::cpu>;
-    using nb_2d_double = nb::ndarray<double, nb::ndim<2>, nb::c_contig, nb::device::cpu>;
-   
-    // Constructor
-    EndEffectorIdentificationMomentumPybindWrapper() = default;
+  using nb_1d_double =
+      nb::ndarray<double, nb::ndim<1>, nb::c_contig, nb::device::cpu>;
+  using nb_2d_double =
+      nb::ndarray<double, nb::ndim<2>, nb::c_contig, nb::device::cpu>;
 
-    EndEffectorIdentificationMomentumPybindWrapper(const std::string urdf_filename,
-                                                   const nb_1d_double friction_parameters_input,
-                                                   const int H_input,
-                                                   const std::string time_format_string,
-                                                   const bool display_info);
+  // Constructor
+  EndEffectorIdentificationMomentumPybindWrapper() = default;
 
-    // Destructor
-    ~EndEffectorIdentificationMomentumPybindWrapper() = default;
+  EndEffectorIdentificationMomentumPybindWrapper(
+      const std::string urdf_filename,
+      const nb_1d_double friction_parameters_input, const int H_input,
+      const std::string time_format_string, const bool display_info);
 
-    // class methods
-    void set_ipopt_parameters(const double tol,
-                              const double max_wall_time,
-                              const int print_level,
-                              const int max_iter,
-                              const std::string mu_strategy,
-                              const std::string linear_solver,
-                              const bool gradient_check);
+  // Destructor
+  ~EndEffectorIdentificationMomentumPybindWrapper() = default;
 
-    void add_trajectory_file(const std::string filename_input);
+  // class methods
+  void set_ipopt_parameters(const double tol, const double max_wall_time,
+                            const int print_level, const int max_iter,
+                            const std::string mu_strategy,
+                            const std::string linear_solver,
+                            const bool gradient_check);
 
-    nb::tuple optimize();
+  void add_trajectory_file(const std::string filename_input);
 
-    void reset();
+  nb::tuple optimize();
 
-    // class members
-        // robot model
-    Model model;
-    VecX offset;
+  void reset();
 
-        // trajectory information
-    TimeFormat time_format;
+  // class members
+  // robot model
+  Model model;
+  VecX offset;
 
-        // system identification settings
-    int H = 10; // forward integration horizon
-    int downsample_rate = 1; // 1 means no downsample
+  // trajectory information
+  TimeFormat time_format;
 
-    SmartPtr<EndEffectorParametersIdentificationMomentum> mynlp;
-    SmartPtr<IpoptApplication> app;
+  // system identification settings
+  int H = 10;              // forward integration horizon
+  int downsample_rate = 1; // 1 means no downsample
 
-    SensorNoiseInfo sensor_noise;
+  SmartPtr<EndEffectorParametersIdentificationMomentum> mynlp;
+  SmartPtr<IpoptApplication> app;
 
-    // Flags to check if the parameters are set
-    bool set_ipopt_parameters_check = false;
+  SensorNoiseInfo sensor_noise;
+
+  // Flags to check if the parameters are set
+  bool set_ipopt_parameters_check = false;
 };
 
 }; // namespace Kinova
