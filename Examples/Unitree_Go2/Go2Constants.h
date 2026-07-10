3308cf87f891c1556e1052bac4d6ba0c281e6cbe
Akshunn <trivediakshunn@gmail.com>
2026-07-10 23:14:43 +0000

    fix: updated joint limits for go2

--- Examples/Unitree_Go2/Go2Constants.h
+++ Examples/Unitree_Go2/Go2Constants.h
@@ -1,4 +1,4 @@
-/// TODO: Values not changed for Go2 specific to do
+// Limits taken from URDF 
 #ifndef GO2_CONSTANTS_H
 #define GO2_CONSTANTS_H
 
@@ -8,21 +8,21 @@ namespace Go2 {
 constexpr int NUM_JOINTS = 3;
 
 constexpr double JOINT_LIMITS_LOWER[NUM_JOINTS] = {
-    -49.45,
-    -39.3,
-    -161.46, // FR (hip, thigh, calf) (-0.863, -.686, -2.818)
+    -60.0,
+    -90.0,
+    -156.0, // FR (hip, thigh, calf) (-1.0472, -1.5708, -2.7227) 
 };
 
 constexpr double JOINT_LIMITS_UPPER[NUM_JOINTS] = {
-    49.45,
-    257.9,
-    -50.88, // FR (0.863, 4.501, -0.888)
+    60,
+    200,
+    -48, // FR (1.0472, 3.4907, -0.83776)
 };
 
 constexpr double VELOCITY_LIMITS_LOWER[NUM_JOINTS] = {-1724.6, -1724.6,
-                                                      -1149.3};
+                                                      -899.5};
 
-constexpr double VELOCITY_LIMITS_UPPER[NUM_JOINTS] = {1724.6, 1724.6, 1149.3};
+constexpr double VELOCITY_LIMITS_UPPER[NUM_JOINTS] = {1724.6, 1724.6, 899.5};
 
 constexpr double ACCELERATION_LIMITS_LOWER[NUM_JOINTS] = {-10000.0, -10000.0,
                                                           -10000.0};
@@ -30,9 +30,9 @@ constexpr double ACCELERATION_LIMITS_LOWER[NUM_JOINTS] = {-10000.0, -10000.0,
 constexpr double ACCELERATION_LIMITS_UPPER[NUM_JOINTS] = {10000.0, 10000.0,
                                                           10000.0};
 
-constexpr double TORQUE_LIMITS_LOWER[NUM_JOINTS] = {-23.7, -23.7, -35.55};
+constexpr double TORQUE_LIMITS_LOWER[NUM_JOINTS] = {-23.7, -23.7, -45.4};
 
-constexpr double TORQUE_LIMITS_UPPER[NUM_JOINTS] = {23.7, 23.7, 35.55};
+constexpr double TORQUE_LIMITS_UPPER[NUM_JOINTS] = {23.7, 23.7, 45.4};
 
 constexpr double GRAVITY = -9.81; // m/s^2
 
@@ -42,11 +42,6 @@ constexpr double GRAVITY = -9.81; // m/s^2
 //               RR_hip, RR_thigh, RR_calf,
 //               RL_hip, RL_thigh, RL_calf]
 //
-// URDF limits (radians, from go1.urdf):
-//   hip   (abduction): lower=-0.863  upper=+0.863
-//   thigh (pitch):     lower=-0.686  upper=+4.501
-//   calf  (pitch):     lower=-2.818  upper=-0.888
-
 // Standard standing pose (body ~0.265 m above ground).
 // Foot Z ≈ -0.265 m relative to body origin.
 // Used as the excitation center for friction SysID.
